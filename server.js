6604fa1ae83fce063ee31f7a74fb70e46c577a58
Smile <105898872+Big-Smile-5@users.noreply.github.com>
2022-06-26 03:19:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// PUBLICKEY = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-// PRIVATEKEY = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-// INFURA_URL = "http://localhost:8545"
 
 const express = require('express')
 const app = express()
